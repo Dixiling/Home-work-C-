5c985b67c59d6ccced6d70e1e0abc944d9635f0d
Конпаку <sputnik-999-mel@mail.ru>
2023-08-12 10:48:47 +0500

    задание №8

---
+++ HW8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("");
+Console.WriteLine("Задание 8");
+Console.Write("Введите число :");
+int num7 = Convert.ToInt32(Console.ReadLine());
+int evenNumber = 2;
+
+if(num7 > 1)
+{
+    while(evenNumber <= num7)
+    {
+        Console.Write($"{evenNumber} ");
+        evenNumber = evenNumber + 2;
+    }
+}
