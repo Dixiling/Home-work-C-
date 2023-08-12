262cd7d41c7718d0e37c330cf50fc3a4f2195fd3
Конпаку <sputnik-999-mel@mail.ru>
2023-08-12 10:44:04 +0500

    задание №4

---
+++ HW4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("");
+Console.WriteLine("Задание 4");
+Console.Write("введите первое целое число :");
+int num3 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите второе целое число :");
+int num4 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите третье целое число :");
+int num5 = Convert.ToInt32(Console.ReadLine());
+int Mmax =num3 ;
+if (num4 > num3)
+    Mmax = num4;
+if (num5 > Mmax)
+    Mmax = num5;
+Console.Write($"Получаем максимальное число:{Mmax}");
