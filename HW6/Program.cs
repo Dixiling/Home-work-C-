80d5189ae4fe0701721378e579bca4be83185984
Конпаку <sputnik-999-mel@mail.ru>
2023-08-12 10:46:54 +0500

    задание №6

---
+++ HW6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("");
+Console.WriteLine("Задание 6");
+Console.Write("введите первое целое число :");
+int num6 = Convert.ToInt32(Console.ReadLine());
+int chet =  num6 % 2 ;
+if (chet ==0)
+    Console.WriteLine("Число чётное") ;
+else 
+    Console.WriteLine ("Число нечётное") ;
