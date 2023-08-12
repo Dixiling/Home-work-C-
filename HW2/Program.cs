0af57ff99b4fe61d41d1ce42fdc67f740663f87e
Конпаку <sputnik-999-mel@mail.ru>
2023-08-12 10:39:16 +0500

    задание №2

---
+++ HW2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Задание 2");
+Console.Write("введите первое целое число :");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите второе целое число :");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max = num1 ;
+if (num1 >= num2) 
+    Console.WriteLine($"Первое число больше чем второе :{max} > {num2}");
+else
+    Console.WriteLine($"Второе число больше чем первое :{num2} > {max}");
+    max = num2 ;
